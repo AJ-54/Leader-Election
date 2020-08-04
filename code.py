8e46c2011690b461b287da38aac154bf8370dcf2
AJ-54 <ayushjai@iitg.ac.in>
2020-08-05 02:16:38 +0530

    Final running version

--- code.py
+++ code.py
@@ -4,12 +4,11 @@ import time
 from threading import Event
 
 # The current leader is going to be the value with this key.
-LEADER_KEY = "/ayush-demo/leader"
+LEADER_KEY = "/leader"
 
 # Entrypoint of the program
 def main(server_name):
     # Create a new client to etcd.
-    print(server_name)
     client = etcd3.client(host="localhost",port=2379)
 
     while True:
@@ -52,29 +51,28 @@ def on_leadership_gained(lease):
 
         try:
             print("Refreshing lease; still the leader")
-            # lease.refresh()
+            lease.refresh()
+            print("Passing next stage")
             #  Here we can add business logic
-            do_work(lease)
-        
-        except KeyboardInterrupt:
-            print ("\nRevoking lease, no longer the leader.")
-            lease.revoke()
-            sys.exit(1)
+            do_work()
         
         except Exception:
             # Here we most likely got a client timeout
             # maybe frome network issues. Try to revoke 
             # the current lease so another member can get
             # leadership.
-            print("this is wrong")
             lease.revoke()
             return
+        
+        except KeyboardInterrupt:
+            print ("\nRevoking lease, no longer the leader.")
+            lease.revoke()
+            sys.exit(1)
         
 def wait_for_next_election(client):
     election_event = Event()
 
-    def watch_callback(resp):
-        print(resp)
+    def watch_callback(resp):   
         for event in resp.events:
             # For each event in watch event, if the event is a deletion
             # it means the kep expired / got deleted, which means the leadership
@@ -92,6 +90,9 @@ def wait_for_next_election(client):
     except KeyboardInterrupt:
         client.cancel_watch(watch_id)
         sys.exit(1)
+    
+    # Cancel the watch; we see that election should happen again
+    client.cancel_watch(watch_id)
 
 # Try to insert a key into etcd with a value and a lease.If the lease expires
 # that key will get automatically deleted behind the scenes.If that key 
@@ -104,13 +105,9 @@ def try_insert(client, key, value, lease):
     )
     return insert_succeeded
 
-def do_work(lease):
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print ("\nRevoking lease, no longer the leader.")
-        lease.revoke()
-        sys.exit(1)
+def do_work():
+    time.sleep(1)
+
 
 if __name__ == "__main__":
     server_name = sys.argv[1]
